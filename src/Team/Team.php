be919f0090ec72da10bcb01d29dd1a6f079b0414
rhare <rob.j.hare@gmail.com>
2018-05-12 17:33:08 -0700

    Rhare (#8)
    * Renmaed home_page.php to index.php. Added Database and User classes.
    
    * Fixed database connect_by_role function
    
    * Added constructor to the datatabase class
    
    * Updated schema. Added site layout. Updated DB class
    
    * Updated User class to not have a UserName
    
    * Fixing merge conflicts
    
    * Updated database settings. DB is now a AWS RDS
    
    * Updated schema
    
    * Renamed directories to better URL representation. Added a simple nav bar. Added bootstrap. Added navbar.
    
    * fixed broken require_once
    
    * Major changes. Changed the site to track teams instead of just one team.
    
    * Fixing merge conflicts
    
    * removed team

--- src/Team/Team.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Implement Team class here. It will represent an instance of the Team
-// This may not be needed
-// I'm thinking the team page will use this class to represent all players on the team into a "Team" object.
-?>
